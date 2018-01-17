0f2d01caec1ee801906a86b50b0e4bdab51511c4
Sunil Biradar <sunilbiradar@Cambrias-MacBook-Pro.local>
2018-01-17 11:16:25 -0800

    add test cases

--- test/javascript/facility/children_tests.js
+++ test/javascript/facility/children_tests.js
@@ -5,19 +5,22 @@ import ShallowRenderer from 'react-test-renderer/shallow'
 describe('Verify Children Component', function () {
   const props = {
     facilityData: {
-      count: 3,
-      children: [{
-        id: 2222,
-        person: {
-          age: 17,
-          date_of_birth: '2000-05-28',
-          first_name: 'Mei',
-          gender: 'F',
-          last_name: 'Takahashi'
+      'count': 3,
+      'children': [{
+        'id': 2222,
+        'person': {
+          'age': 17,
+          'date_of_birth': '2000-05-28',
+          'first_name': 'Mei',
+          'gender': 'F',
+          'last_name': 'Takahashi'
         },
-        date_of_placement: '01/02/2003',
-        assigned_worker: 'asdfg',
-        county_of_origin: 'sacramento'
+        'date_of_placement': '01/02/2003',
+        'assigned_worker': {
+          'first_name': 'Peter',
+          'last_name': 'Parker'
+        },
+        'county_of_origin': 'sacramento'
       }]}
   }
   const renderChildComp = new ShallowRenderer()
@@ -25,5 +28,41 @@ describe('Verify Children Component', function () {
   const resultTag = childComp.props
   it('check Children table', function () {
     expect(resultTag.className).toBe('facility-children col-xs-12 col-sm-12 col-md-12 col-lg-12')
+  })
+  it('Verify ID', function () {
+    let childID = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[0].props.children[1]
+    expect(childID).toBe(2222)
+  })
+  it('Verify child first name', function () {
+    let childFirstName = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[1].props.children[1]
+    expect(childFirstName).toBe('Mei')
+  })
+  it('Verify child last name', function () {
+    let childLastName = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[2].props.children[1]
+    expect(childLastName).toBe('Takahashi')
+  })
+  it('Verify child gender/sex', function () {
+    let childsGender = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[3].props.children[1]
+    expect(childsGender).toBe('F')
+  })
+  it('Verify child age', function () {
+    let childsAge = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[4].props.children[1]
+    expect(childsAge).toBe(17)
+  })
+  it('Verify child date of birth', function () {
+    let childDateOfBirth = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[5].props.children[1]
+    expect(childDateOfBirth).toBe('2000-05-28')
+  })
+  it('Verify date of placement', function () {
+    let dateOfPlacement = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[6].props.children[1]
+    expect(dateOfPlacement).toBe('01/02/2003')
+  })
+  it('Verify assigned worker full name', function () {
+    let assignedWorkerFullName = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[7].props.children[1]
+    expect(assignedWorkerFullName).toBe('Peter Parker')
+  })
+  it('Verify child county of origin', function () {
+    let countyOfOrigin = childComp.props.children.props.children[1].props.children[1].props.children[0].props.children[8].props.children[1]
+    expect(countyOfOrigin).toBe('sacramento')
   })
 })
--- test/javascript/facility/facilityComplaints_tests.js
+++ test/javascript/facility/facilityComplaints_tests.js
@@ -2,26 +2,55 @@ import React from 'react'
 import FacilityComplaints from '../../../app/javascript/facility/complaints.jsx'
 import ShallowRenderer from 'react-test-renderer/shallow'
 
-describe('Facility Complaints Section', function () {
+describe('Verify Complaints Component', function () {
   const props = {
     facilityData: {
-      complaints: [
+      'count': 3,
+      'complaints': [
         {
-          approval_date: '2010-03-05',
-          assigned_worker: 'Jan Doe',
-          complaint_date: '2010-02-03',
-          control_number: '12-CR20161205112053',
-          id: null,
-          priority_level: 2,
-          status: 'Pending'
+          'id': 'dlf0245',
+          'complaint_date': '2016-09-27 00:00:00',
+          'assigned_worker': 'Harry Potter',
+          'control_number': '19-CR-20160927081411',
+          'priority_level': '2',
+          'status': 'Approved',
+          'approval_date': '2016-12-10 00:00:00'
         }
       ]
     }
   }
-  const renderComplaints = new ShallowRenderer()
-  const complaintsComp = renderComplaints.render(<FacilityComplaints {...props} />)
-  const resultComp = complaintsComp
-  it('Verify Facility Complaints Table', function () {
-    expect(resultComp.props.className).toBe('facility-children col-xs-12 col-sm-12 col-md-12 col-lg-12')
+  const renderComplaintsComp = new ShallowRenderer()
+  const complaintsComp = renderComplaintsComp.render(<FacilityComplaints {...props} />)
+  const resultTag = complaintsComp.props
+  it('check Complaints table', function () {
+    expect(resultTag.className).toBe('facility-children col-xs-12 col-sm-12 col-md-12 col-lg-12')
+  })
+  it('Verify Complaint ID', function () {
+    let complaintId = complaintsComp.props.children.props.children[1].props.children[1].props.children[0].props.children[0].props.children[1]
+    expect(complaintId).toBe('dlf0245')
+  })
+  it('Verify complaint date', function () {
+    let complaintDate = complaintsComp.props.children.props.children[1].props.children[1].props.children[0].props.children[1].props.children[1]
+    expect(complaintDate).toBe('2016-09-27 00:00:00')
+  })
+  it('Verify assigned worker full name', function () {
+    let assignedWorkerFullName = complaintsComp.props.children.props.children[1].props.children[1].props.children[0].props.children[2].props.children[1]
+    expect(assignedWorkerFullName).toBe('Harry Potter')
+  })
+  it('Verify complaint control number', function () {
+    let complaintControlNumber = complaintsComp.props.children.props.children[1].props.children[1].props.children[0].props.children[3].props.children[1]
+    expect(complaintControlNumber).toBe('19-CR-20160927081411')
+  })
+  it('Verify priority level', function () {
+    let priorityLevel = complaintsComp.props.children.props.children[1].props.children[1].props.children[0].props.children[4].props.children[1]
+    expect(priorityLevel).toBe('2')
+  })
+  it('Verify complaint status', function () {
+    let complaintStatus = complaintsComp.props.children.props.children[1].props.children[1].props.children[0].props.children[5].props.children[1]
+    expect(complaintStatus).toBe('Approved')
+  })
+  it('Verify complaint approval date', function () {
+    let complaintApprovalDate = complaintsComp.props.children.props.children[1].props.children[1].props.children[0].props.children[6].props.children[1]
+    expect(complaintApprovalDate).toBe('2016-12-10 00:00:00')
   })
 })
